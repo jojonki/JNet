8b5ba123c3df1b39051cb7cfc54bbf9c927d299f
jojonki <junki.ohmura@gmail.com>
2017-12-02 10:31:57 -0500

    fix test func

--- train.py
+++ train.py
@@ -59,12 +59,14 @@ args = parser.parse_args()
 for arg in vars(args):
     print('Argument:', arg, getattr(args, arg))
 
-# train_data = load_processed_data('./dataset/train.txt')
+train_data = load_processed_data('./dataset/train.txt')
 # save_pickle(train_data, 'train_data.pickle')
-train_data = load_pickle('train_data.pickle')
-# dev_data = load_processed_data('./dataset/dev.txt')
+# train_data = load_pickle('train_data.pickle')
+dev_data = load_processed_data('./dataset/dev.txt')
 # save_pickle(dev_data, 'dev_data.pickle')
-dev_data = load_pickle('dev_data.pickle')
+# dev_data = load_pickle('dev_data.pickle')
+print('train_data', len(train_data))
+print('dev_data', len(dev_data))
 data = train_data + dev_data
 # vocab = set()
 # for _, ctx, query, _, _ in data:
@@ -84,8 +86,8 @@ print('query_token_maxlen:', query_token_maxlen)
 args.answer_token_len = 1 # 2 TODO
 
 
-args.pre_embd = load_pickle('./pickles/glove_embd.pickle')
-# args.pre_embd = torch.from_numpy(load_glove_weights('./dataset', args.embd_size, len(vocab), w2i)).type(torch.FloatTensor)
+# args.pre_embd = load_pickle('./pickles/glove_embd.pickle')
+args.pre_embd = torch.from_numpy(load_glove_weights('./dataset', args.embd_size, len(vocab), w2i)).type(torch.FloatTensor)
 # save_pickle(args.pre_embd, './pickles/glove_embd.pickle')
 
 
@@ -102,7 +104,7 @@ def train(data, model, optimizer, loss_fn, n_epoch=5, start_epoch=0, batch_size=
             batch_data = data[i:i+batch_size]
             c = [d[1] for d in batch_data]
             q = [d[2] for d in batch_data]
-            a_txt = [d[4] for d in batch_data]
+            # a_txt = [d[4] for d in batch_data]
             b_ctx_token_maxlen = max([len(cc) for cc in c])
             b_query_token_maxlen = max([len(qq) for qq in q])
             # print('BATCH context maxlen: {}, query maxlen: {}'.format(b_ctx_token_maxlen, b_query_token_maxlen))
@@ -140,21 +142,21 @@ def train(data, model, optimizer, loss_fn, n_epoch=5, start_epoch=0, batch_size=
             optimizer.step()
 
         # end epoch
-        _, preds = torch.max(outs, 1)
-        ct = 0
-        for pred, label in zip(preds, labels):
-            if pred.data[0] == label.data[0]:
-                ct += 1
-        debug_log('Current Acc: {:.2f}% ({}/{})'.format(ct/len(preds), ct, len(preds)))
+        # _, preds = torch.max(outs, 1)
+        # ct = 0
+        # for pred, label in zip(preds, labels):
+        #     if pred.data[0] == label.data[0]:
+        #         ct += 1
+        # debug_log('Current Acc: {:.2f}% ({}/{})'.format(ct/len(preds), ct, len(preds)))
 
-        # filename = '{}/{}_Epoch-{}.model'.format(args.output_dir, now(), epoch)
-        # save_checkpoint({
-        #     'epoch': epoch + 1,
-        #     'state_dict': model.state_dict(),
-        #     'optimizer' : optimizer.state_dict(),
-        # }, is_best=True, filename=filename)
+        filename = '{}/{}_Epoch-{}.model'.format(args.output_dir, now(), epoch)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'optimizer' : optimizer.state_dict(),
+        }, is_best=True, filename=filename)
 
-    # save_pickle(losses, '{}/{}_train_losses.pickle'.format(args.output_dir, now()))
+    save_pickle(losses, '{}/{}_train_losses.pickle'.format(args.output_dir, now()))
 
 
 # def test {{{
@@ -182,21 +184,22 @@ def test(data, model, batch_size=32):
             print(loss.data[0])
 
         _, preds = torch.max(outs, 1)
-        # correct += torch.sum(outs, labels).data[0]
-        already_saved = False
-        for j, (pred, label) in enumerate(zip(preds, labels)):
-            c_label = batch_data[j][0]
-            if pred.data[0] == label.data[0]:
-                correct += 1
-                save_fig_file = '{}/{}_TEST_{}_bs-{}_correct{}.png'.format(args.output_dir, now(), c_label, i, j)
-            else:
-                save_fig_file = '{}/{}_TEST_{}_bs-{}_wrong{}.png'.format(args.output_dir, now(), c_label, i, j)
-            ans = batch_data[j][3]
-            if not already_saved:
-                # test_img_data = (c[j], q[j], attens[j], ans, save_fig_file)
-                # save_pickle(test_img_data, 'test_img_data.pickle')
-                plot_heat_matrix(c[j], q[j], attens[j], ans, output_file=save_fig_file, title='Answer: '+a_txt[j], pred=pred.data[0])
-            already_saved = True
+        correct += torch.sum(preds == labels).data[0]
+        print(correct, '/', batch_size)
+        # already_saved = False
+        # for j, (pred, label) in enumerate(zip(preds, labels)):
+        #     c_label = batch_data[j][0]
+        #     if pred.data[0] == label.data[0]:
+        #         correct += 1
+        #         save_fig_file = '{}/{}_TEST_{}_bs-{}_correct{}.png'.format(args.output_dir, now(), c_label, i, j)
+        #     else:
+        #         save_fig_file = '{}/{}_TEST_{}_bs-{}_wrong{}.png'.format(args.output_dir, now(), c_label, i, j)
+        #     ans = batch_data[j][3]
+        #     if not already_saved:
+        #         # test_img_data = (c[j], q[j], attens[j], ans, save_fig_file)
+        #         # save_pickle(test_img_data, 'test_img_data.pickle')
+        #         plot_heat_matrix(c[j], q[j], attens[j], ans, output_file=save_fig_file, title='Answer: '+a_txt[j], pred=pred.data[0])
+        #     already_saved = True
         total += batch_size
     print('Test Acc: {:.2f}% ({}/{})'.format(correct/total, correct, total))
 # }}}
